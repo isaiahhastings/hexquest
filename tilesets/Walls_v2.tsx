0131b06b110873349c321eaa1f0d3ea13b120f2e
gamesbypendragon <gamesbypendragon@gmail.com>
2025-11-30 13:42:58 -0600

    Map Blockout
    Testing map blockout + adding placeholder assets

---
+++ tilesets/Walls_v2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Walls_v2" tilewidth="32" tileheight="32" tilecount="29211" columns="91">
+ <image source="../../../../Library/CloudStorage/Dropbox/Misc/GatherTown/GatherSharedAssets/Gather_Walls_v2.png" width="2912" height="10272"/>
+</tileset>
